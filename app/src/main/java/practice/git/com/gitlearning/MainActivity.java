31ee32c80c47dd2bf375ab1b6981a66e575bbcc8
Bhujatha <bujatha01@gmail.com>
2018-02-12 23:24:46 +0530

    added ImageView

--- app/src/main/java/practice/git/com/gitlearning/MainActivity.java
+++ app/src/main/java/practice/git/com/gitlearning/MainActivity.java
@@ -2,11 +2,13 @@ package practice.git.com.gitlearning;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv;
+    ImageView iv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
